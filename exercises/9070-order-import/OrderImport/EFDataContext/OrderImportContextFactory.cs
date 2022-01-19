2521f831dcc5d31f74c2a6b273061d41588bc815
Zoltan Toth <toth.tozso.zoltan@gmail.com>
2022-01-19 10:56:57 +0100

    EF ChuckNorrisExcercise: Project Added

---
+++ exercises/9070-order-import/OrderImport/EFDataContext/OrderImportContextFactory.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderImport.EFDataContext
+{
+    class OrderImportContextFactory : IDesignTimeDbContextFactory<OrderImportContext>
+    {
+        public OrderImportContext CreateDbContext(string[] args = null)
+        {
+            var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<OrderImportContext>();
+            optionsBuilder
+                // Uncomment the following line if you want to print generated
+                // SQL statements on the console.
+                //.UseLoggerFactory(LoggerFactory.Create(builder => builder.AddConsole()))
+                .UseSqlServer(configuration["ConnectionStrings:DefaultConnection"]);
+
+            return new OrderImportContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ exercises/9070-order-import/OrderImport/Models/Order.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OrderImport.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+
+        public int CustomerId { get; set; }
+
+        public Customer Customer { get; set; }
+
+        [Column(TypeName = "Datetime2")]
+        public DateTime OrderDate { get; set; }
+
+        [Column(TypeName = "decimal(8,2)")]
+        public decimal OrderValue { get; set; }
+    }
+}
